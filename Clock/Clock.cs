a0d6e6526862966683c8091d39c8a7a082ec12ba
Balonich <balonich@gmail.com>
2021-04-14 21:20:22 +0300

    Implemented Clock class in Clock Lib

--- Clock/Clock.cs
+++ Clock/Clock.cs
@@ -1,7 +1,55 @@
 namespace Clock
 {
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
     public class Clock
     {
-        
+        private CancellationTokenSource cancellationToken;
+
+        public Clock()
+        {
+            CurrentTime = DateTime.UtcNow;
+            Logger = (message) => Console.WriteLine(message);
+            IsRunning = false;
+        }
+
+        public Action<string> Logger { get; set; }
+
+        public DateTime CurrentTime { get; private set; }
+
+        public bool IsRunning { get; set; }
+
+        public void Start(Action<DateTime> output)
+        {
+            if (IsRunning)
+            {
+                Logger($"{CurrentTime}: The process is still running");
+            }
+            else
+            {
+                cancellationToken = new CancellationTokenSource();
+                IsRunning = true;
+
+                Task.Run(() =>
+                {
+                    while (!cancellationToken.Token.IsCancellationRequested)
+                    {
+                        CurrentTime = DateTime.UtcNow;
+                        Thread.Sleep(1000);
+                        output(CurrentTime);
+                    }
+
+                    Logger($"{CurrentTime}: Canceled");
+                });
+            }
+        }
+
+        public void Stop()
+        {
+            cancellationToken.Cancel();
+            IsRunning = false;
+        }
     }
 }
