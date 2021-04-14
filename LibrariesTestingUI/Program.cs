92a280fd3ff659468d60f9664a3ea3548b114345
Balonich <balonich@gmail.com>
2021-04-14 21:09:59 +0300

    Initial Commit. Created classlibs, console app and sln

---
+++ LibrariesTestingUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibrariesTestingUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Logger/Logger.cs
@@ -0,0 +1,7 @@
+namespace Logger
+{
+    public class Logger
+    {
+        
+    }
+}
